c35a6381ae557864f26d5ebae0da964201472d1a
Fabio Carlesso <fabio.carlesso@gmail.com>
2020-07-29 08:40:53 -0300

    Ajustado para PageDefault,css e criado categorias.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css'
-import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+import { Link } from 'react-router-dom';
 
 function Menu(){
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt='Fabioflix logo' />
-            </a>
-            <ButtonLink className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
-            </ButtonLink>
+            </Button>
         </nav>
     );
 }
